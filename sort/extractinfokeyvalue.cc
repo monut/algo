5d4612d48bbb5b68014f91b60ee23bddaaaf9e6e
monut <deepakt@hotmail.com>
2018-07-26 14:12:34 -0700

    Create extractinfokeyvalue.cc

---
+++ sort/extractinfokeyvalue.cc
@@ -0,0 +1,38 @@
+
+//#include <regex>
+/*
+    hash map of {key => {count, <highest string>}}
+    
+*/
+vector<string> solve(vector<string> arr) {
+    unordered_map<string, pair<int, string> > hmap;
+   
+    for(int i = 0; i < arr.size(); i++){
+        string s = arr[i];
+        vector<string> tok;
+        regex re(" ");
+        copy(sregex_token_iterator(s.begin(), s.end(), re, -1), 
+                sregex_token_iterator(), back_inserter(tok));
+        // find key 
+        auto it = hmap.find(tok[0]);
+        if(it == hmap.end()){
+            hmap[tok[0]] = {1, tok[1]};
+        } else {
+            int cnt = it->second.first + 1;
+            string val = (it->second.second > tok[1])? it->second.second : tok[1];
+            hmap[tok[0]] = {cnt, val};
+        }
+        
+    }
+    
+    vector<string> res;
+    for(auto& hp : hmap){
+        string s = hp.first + ":" + to_string(hp.second.first) + "," + hp.second.second;
+        res.push_back(s);
+    }
+    return res;
+}
+/*
+Input {"Mark zuck", "Tim cook", "Mark twain"}
+Output {"Mark:2,zuck", "Tim:1,cook"}
+*/
