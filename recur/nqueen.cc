d44a800daaaa8ddff1eac7c41752113749beabd6
monut <deepakt@hotmail.com>
2018-08-02 19:49:11 -0700

    Update nqueen.cc

--- recur/nqueen.cc
+++ recur/nqueen.cc
@@ -1,77 +1,62 @@
-#include "myutils.h"
 
-using namespace std;
 /*
- * Complete the function below.
+ recursively go through each row starting from 0. and for row, 
+ check all the columns in which Q can be placed. if good then place the queen
+ and recursively check the next rows.  A cell is good if no no queen in rows
+ above and the same column. Left diaginol (r--,c--) and right diagonal (r--, c++).
  */
 
-bool isgood(vector<string>& ires, int r, int c, int n){
-    // check if this is valid
-    if(r == 0){
+
+bool 
+isgood(vector<string>& ch, int r, int c){
+    if(r == 0)
         return true;
+        
+    int num_rows = ch.size();
+    int num_cols = ch[0].size();
+    // check in rows above for the column
+    for(int i = 0 ; i < r; i++){
+        if(ch[i][c] == 'q'){
+            return false;
+        }
     }
-    // check in each row if we have a queen in same column
-    for(int a = 0; a < r; a++){
-        if(ires[a][c] != '_')
+    
+    // check in the diagonal left
+    int i = r, j = c;
+    while(i >=0 && j >= 0){ // check i >= 0 
+        if(ch[i--][j--] == 'q')
             return false;
     }
-    // left to right diagonal
-    int i,j;
-    for(i = r, j = c; i>= 0 && j >= 0; i--, j--){
-        // diagonal l to r
-            if(ires[i][j] != '_')
-                return false;
-    } 
-        // diagnol to right top corner to l
-    for(int i = r, j = c; i >= 0 && j < n ; i--, j++){
-        if(ires[i][j] != '_')
+    
+    // check in the diagonal right
+    i = r, j = c;
+    while(i >= 0 && j < num_cols){ // check for i >= 0
+        if(ch[i--][j++] == 'q')
             return false;
     }
     return true;
- }
-void
-helper(vector< vector<string> >& res, vector<string>& ires, int r, int pos, int n){
-    if(r == n) {
-        res.push_back(ires);
-        return;
-    }
-    for(auto c = pos; c < n; c++){
-        if(isgood(ires, r, c, n)) {
-            // go on 
-            ires[r][c] = 'q';
-            helper(res, ires, r+1, 0,  n);
-            ires[r][c] = '_';
-        }
-    }
- }
-
-vector < vector<string> > find_all_arrangements(int n) {
-   vector< vector<string> > res;
-   string each_row(n, '_');
-   vector<string> chess(n, each_row);
-   helper(res,chess, 0, 0, n);
-   cout << res;
-   return res;
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    vector < vector<string> > res;
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    res = find_all_arrangements(n);
-    for(int res_i = 0; res_i < res.size(); res_i++) {
-        for(int res_j = 0; res_j < res[res_i].size(); res_j++) {
-            fout << res[res_i][res_j] << " ";;
-        }
-        fout << endl;
+void helper(vector<vector<string> >& res, vector<string> ch, int r, int n){
+    if(r == n){
+        res.push_back(ch);
     }
+    
+    for(int c = 0; c < n; c++){
+        if(isgood(ch,r,c)) {
+            // choose it and move to next row
+            ch[r][c] = 'q';
+            helper(res, ch, r+1, n);
+            // reset
+            ch[r][c] = '-';
+        }
+    }
+}
 
-    fout.close();
-    return 0;
+vector <vector<string> > find_all_arrangements(int n) {
+    vector< vector<string>> res;
+    vector<string> chess(n, string(n,'-'));
+    helper(res, chess, 0, n);
+    return res;   
 }
 
