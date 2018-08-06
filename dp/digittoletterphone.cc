6c1a0e7944bc16c24a38ee388e06be3aa0a194a7
monut <deepakt@hotmail.com>
2018-08-05 17:33:47 -0700

    Create digittoletterphone.cc

---
+++ dp/digittoletterphone.cc
@@ -0,0 +1,46 @@
+/* 
+Given a 7 digit number find all possible leter combinatns based on digit to letter mapping on numeric pad
+*/
+#include <iostream>
+#include <unordered_map>
+#inlcude <vector>
+#inlcude <string>
+
+using namespace std;
+
+
+unordered_map <int, string> keypad = { {1, ""}, {2, "abc"}, {2, "def"}, {3,"ghi"}, {4,"jkl"}, {5,"mno"},
+  {6, "pqrs"}, {7, "tuv"}, {8, "wxyz"}};
+ 
+unordered_set<string> dict = {"dad", "bad","cattle"}; 
+
+void 
+helper(vector<int>& numb, i , string exp, vector<string>& res) {
+  if(i == number.length()) {
+    // check in dictionary and if found insert in the result
+    if(dict.find(exp) != dict.end()){
+      res.push_back(exp);
+    }
+    return;
+  }
+  // get the string assocaited with the number
+  string st = keypad[number[i]];
+  for(auto c : st) {
+    helper(numb, i+1, exp + c);
+  }
+ }
+ 
+ int main(){
+  vector<string> res;
+  vector<int> num1 = {3,2,3};
+  vector<int> num2  = {2,2,8,8,5,3};
+  string exp;
+  helper(num1, 0, exp,res);
+  
+  for(auto val : res) {
+    cout << val << endl;
+  }
+ 
+ }
+
+
