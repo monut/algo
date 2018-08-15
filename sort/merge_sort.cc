364cff892d307af164c9a55dea1b075c584f2d73
monut <deepakt@hotmail.com>
2018-08-15 07:48:54 -0700

    Create merge_sort.cc

---
+++ sort/merge_sort.cc
@@ -0,0 +1,37 @@
+
+/*
+ * Complete the function below.
+ */
+void mymerge(vector<int>& ar, int st, int m,  int en) {
+    int i = st;
+    int j = m+1;
+    vector<int> res;
+    while(i <= m && j <= en) {
+        if(ar[i] < ar[j]){
+            res.push_back(ar[i++]);
+        } else {
+            res.push_back(ar[j++]);
+        }
+    }
+    while(i <= m){
+        res.push_back(ar[i++]);
+    }
+    
+    while(j <= en){
+        res.push_back(ar[j++]);
+    }
+    for(int idx = st; idx <= en; idx++){
+        ar[idx] = res[idx-st];
+    }
+}
+void helper(vector<int>& intArr, int st, int en){
+    if(st >= en) return;
+    int mid = st + (en - st)/2;
+    helper(intArr, st, mid);
+    helper(intArr, mid+1, en);
+    mymerge(intArr, st, mid, en);
+}
+vector <int> MergeSort(vector <int> intArr) {
+    helper(intArr, 0, intArr.size() - 1);
+    return intArr;
+}
