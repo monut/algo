36045d395938edda6b245a03bfa0c07be82846a4
monut <deepakt@hotmail.com>
2018-08-20 09:38:40 -0700

    Update arrayadvance.cc

--- adhoc/arrayadvance.cc
+++ adhoc/arrayadvance.cc
@@ -1,3 +1,11 @@
+/*  Given an array with element indicating the max number of steps one
+    can take from the position given by index. Find if we can reach the end.
+    Indx :      0 1 2 3 4
+    Arry:    <  3 3 0 2 0 >
+   Max <      0 4 5 5 6 6
+   Travers left to right keep the running max and if for any index the max = i 
+   then we cannot reach the end
+ */
 bool arrayadvance(vector<int>& arr){
     int sz = arr.size();
     int max = 0;
