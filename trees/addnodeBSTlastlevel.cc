9620079c3a06ed8e3c3a7c33aa68253415624434
monut <deepakt@hotmail.com>
2018-08-26 16:16:58 -0700

    Update addnodeBSTlastlevel.cc

--- trees/addnodeBSTlastlevel.cc
+++ trees/addnodeBSTlastlevel.cc
@@ -1,16 +1,17 @@
 /* 
 
  */
- void addnode(BT nd, int val) {
+ void addnode(BT *nd, int val) {
   
- queue<BT> q;
+ queue<BT *> q;
  BT *cand = nullptr;
  q.push(nd);
  while(!q.empty()){
   auo val = q.front();
-  if(val->left == nullptr && val->right != nullptr) val->left = new BT(val);
-  if(val->left != nullptr && val->right == nullptr) val->right = new BT(val);
-  if(val->left == nullptr && val->right == nullptr) cand = val;
+  if(val->left == nullptr && val->right != nullptr) { val->left = new BT(val); break; }
+  if(val->left != nullptr && val->right == nullptr)  { val->right = new BT(val); break; }
+  if(val->left == nullptr && val->right == nullptr)  { cand = val; break; }
+  q.push(val->left); q.push(val->right);
   q.pop();
  }
  cand->left = new BT(val);
