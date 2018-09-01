4446c41b740ce5fa7af312b79b1b3f1afb5cc1da
monut <deepakt@hotmail.com>
2018-09-01 09:47:55 -0700

    Update nutbolts.cc

--- sort/nutbolts.cc
+++ sort/nutbolts.cc
@@ -7,56 +7,67 @@ quicksort. Chose value as pivot from nuts and partition bolt on that pivot.
 The parition will retunr the pi for bolt. Now choose the bolt[pi] as pivot 
 and partition the nuts on that pivot.
 */
-#include "myutils.h"
-// max case and min case
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int partition(vector<int>& v, int pvt, int lft , int rt);
-void
-matchit(vector<int>& nuts, vector<int>& bolts, int lft, int rt); 
-
-int
-main() {
-vector<int> nuts = {9, 2, 8, 5,3,4};
-vector<int> bolts = {5, 8, 3, 4, 2, 9};
-
-cout << "Before Nuts " << nuts ;
-cout << endl;
-cout << "Before Bolts " << bolts ;
-cout << endl;
-
-matchit(nuts, bolts,0, nuts.size() - 1);
-
-cout << "After Nuts " << nuts ;
-cout << endl;
-cout << "After Bolts " << bolts ;
-cout << endl;
-}
-
-void
-matchit(vector<int>& nuts, vector<int>& bolts, int lft, int rt) {
-    if(lft < rt) {
-        int pvt = partition(nuts, bolts[rt], lft, rt);
-        partition(bolts,nuts[pvt], lft, rt); 
-        matchit(nuts, bolts, lft, pvt -1);
-        matchit(nuts, bolts, pvt+1, rt);
+template<typename T>
+ostream&
+operator<<(ostream& os, vector<T> v) {
+    for( auto val : v){
+        os << val << " ";
     }
+    return os;
 }
 
-
-int
-partition(vector<int>& v, int pvt, int lft , int rt)
-{
-    int j = lft;
-    for(int i = lft; i < rt; i++) {
-        if(v[i] < pvt){
-            swap(v[i],v[j]);
-            j++;
-        } else if(v[i] == pvt) {
-            swap(v[i], v[rt]);
-            i--;
-        }   
-    } 
-    swap(v[rt], v[j]);
-    return j; 
+// return the index of the pvt
+int 
+partition(vector<int>& v, int pvt, int lft, int rt){
+    while(lft < rt){
+        while(v[lft] < pvt){
+            lft++;
+        }
+        
+        while(v[rt] > pvt){
+            rt--;
+        }
+        
+        if(lft < rt)
+            swap(v[lft], v[rt]);
+    }
+    return rt; // index of pvt element
 }
+
+void matchit(vector<int>& nuts, vector<int>& bolts, int lft, int rt) {
+    if(lft > rt)
+        return;
+    int pvt = nuts[lft];
+    int idx = partition(bolts, pvt, lft, rt);
+    pvt = bolts[idx];
+    partition(nuts, pvt, lft,rt);
+    matchit(nuts, bolts, lft, idx-1);
+    matchit(nuts, bolts, idx+1, rt);
+}
+
+int main() {
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int> nuts = {9, 2, 8, 5,3,4,10};
+    vector<int> bolts = {5, 8, 3, 4, 2, 9, 10}; // No duplicates allowed as per question
+
+    cout << "Before Nuts " << nuts ;
+    cout << endl;
+    cout << "Before Bolts " << bolts ;
+    cout << endl;
+
+    matchit(nuts, bolts,0, nuts.size() - 1);
+
+    cout << "After Nuts " << nuts ;
+    cout << endl;
+    cout << "After Bolts " << bolts ;
+    cout << endl;
+
+    return 0;
+}
+
