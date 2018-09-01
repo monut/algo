9f37b204eda945810f51a820cc9eb8a43149ae01
monut <deepakt@hotmail.com>
2018-09-01 08:16:30 -0700

    Update strtoint.cc

--- misc/strtoint.cc
+++ misc/strtoint.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <ctype.h>
 using namespace std;
 
 int main() {
@@ -13,4 +14,55 @@ int main() {
     int x;
     ss >> x;
     cout << x << endl;
+}
+/*
+    given a string find first contiguous integers 
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+// To execute C++, please define "int main()"
+
+
+int findint(string str){
+    int idx = -1;
+    
+    int i = 0;
+    for(; i < str.length(); i++){
+        if(str[i] == '-' || str[i] == '+' || isdigit(str[i])) {
+           if(idx ==  -1){ 
+               idx = i; 
+           }
+           continue;
+        }
+        if(idx != -1){
+           if (i - idx == 1){
+                if(str[idx] == '-' || str[idx] == '+'){
+                    idx = -1; // reset 
+                    continue;
+                }
+           }    
+           break;
+        }   
+    }
+    if(idx == -1) return 0;
+   
+    int sign = 1;
+    if(str.substr(idx,1) == "-"){
+            sign = -1;
+            idx++;
+    }
+    
+    if(str.substr(idx,1) == "+"){
+            idx++;
+            sign = 1;
+    }
+    return sign * stoi(str.substr(idx, i - idx));
+}
+int main() {
+    // string st("abc23");
+    string st("-abc+23");
+    cout << findint(st);
+        
 }
