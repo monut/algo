f09593d668d0c599c9e7758b5a10006a6c99c515
monut <deepakt@hotmail.com>
2018-09-04 23:31:17 -0700

    Update autocomplete.cc

--- str/autocomplete.cc
+++ str/autocomplete.cc
@@ -2,6 +2,82 @@
   wa2 matched {water, wafer, washington} wa + 2 or more characters.
  */
 
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+// To execute C++, please define "int main()"
+struct TrieNode {
+    map<char,TrieNode * > chlds;
+    bool isword;
+    TrieNode():isword(false){};
+};
+
+void insertTrie(TrieNode *nd, string wrd){
+    for(auto c : wrd){
+        if(nd->chlds.find(c) == nd->chlds.end()){
+            nd->chlds[c] = new TrieNode();
+        }
+        nd = nd->chlds[c];
+    }
+    nd->isword = true;
+}
+
+TrieNode *buildTrie(vector<string>& wrds){
+    TrieNode *root = new TrieNode();
+    for(auto wrd : wrds){
+        insertTrie(root, wrd);
+    } 
+    return root;
+}
+
+void bfs(TrieNode *nd, string exp, int cnt, vector<string>& res){
+   if(!nd ){return;}
+   if(cnt < 0){return;}
+   if(nd->isword && cnt >= 0){
+       res.push_back(exp);
+   }
+    
+   for(auto elem : nd->chlds){
+      bfs(elem.second, exp + elem.first, cnt-1, res);
+   }  
+}
+
+void  helper(string wrd, TrieNode *nd, vector<string>& res){
+    string exp; 
+    for(auto c : wrd){
+       if(isdigit(c)){
+          int cnt = c;
+          bfs(nd, exp, cnt, res);
+       }
+       auto it = nd->chlds.find(c);
+       if(it == nd->chlds.end()){
+           break;
+       }
+       nd = nd->chlds[c];
+       exp +=c;
+    }
+}
+
+int 
+main() {
+    vector<string> vec = {"water", "wafer", "dog"};    
+    TrieNode *root = buildTrie(vec);
+    string wrd = "wa3";
+    vector<string> res;
+    helper(wrd, root, res);
+    
+    for(auto val : res){
+        cout << val << endl;
+    }
+}
+
+
+
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -81,4 +157,5 @@ int main() {
     vector<string> res;
     helper(root, str, 0, exp, res);
 }
+
 
