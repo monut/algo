f3f2de058fe77b40fe772ea2b5507807ebeee1f2
monut <deepakt@hotmail.com>
2018-09-05 21:52:16 -0700

    Update coureschedule.cc

--- graphs/coureschedule.cc
+++ graphs/coureschedule.cc
@@ -1,7 +1,12 @@
 /*
 Topological sort and reverse post order to create a path 
-but first need to verify if there is DAG or nor.
+but first need to verify if there is DAG or not
 */
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
 
 #include <iostream>
 #include <vector>
@@ -12,6 +17,7 @@ using namespace std;
 
 class Solution {
     vector<bool> visited;
+    vector<bool> recur;
     vector<int> path;
 public:
     void explore(int nd, unordered_map<int, vector<int> >& adjl ){
@@ -37,8 +43,33 @@ public:
         }
     }
     
+    bool util(int u, unordered_map<int, vector<int> >& adjl){
+        if(!visited[u]){
+            visited[u] = true;
+            recur[u] = true;
+            for(auto w : adjl[u]){
+               if(!visited[w] && !util(w, adjl)) {
+                   return false;
+               } else if(recur[w]){
+                   return false;
+               }
+            }
+        }    
+        recur[u] = false;
+        return true;
+    }
+        
+    bool isdag(unordered_map<int, vector<int> >& adjl){
+       for(auto elem : adjl){
+           if(!util(elem.first, adjl))
+                    return false;
+       } 
+       return true;
+    }
+    
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
         visited.assign(numCourses, false);
+        recur.assign(numCourses, false);
         cout << " In here ";
         unordered_map<int, vector<int> > adjl;
         for(auto elem : prerequisites){
@@ -46,6 +77,12 @@ public:
             adjl[elem.first].push_back(elem.second);
         }
         
+        if(!isdag(adjl)){
+            cout << " is Not a DAG" << endl;
+            return false;
+        }
+        
+        visited.assign(numCourses, false);
         DFS(adjl);
         
         int i = 0;
@@ -63,7 +100,8 @@ public:
 int main() {
     // vector<int> v = {7, 4, 2, 4, 9};
     int nco = 2;
-    vector<pair<int, int> > vec = {{1,0}};
+    vector<pair<int, int> > vec = {{1,0}, {2,1}};
+    // vector<pair<int, int> > vec = {{1,0}, {0,1}};
     Solution sl;
     sl.canFinish(nco, vec);
 }
