037f7df9f461e22efc3b27601ae791e439d2a4d4
monut <deepakt@hotmail.com>
2018-09-06 12:13:45 -0700

    Update addnodeBSTlastlevel.cc

--- trees/addnodeBSTlastlevel.cc
+++ trees/addnodeBSTlastlevel.cc
@@ -8,8 +8,8 @@
  q.push(nd);
  while(!q.empty()){
   auo val = q.front();
-  if(val->left == nullptr && val->right != nullptr) { val->left = new BT(val); break; }
-  if(val->left != nullptr && val->right == nullptr)  { val->right = new BT(val); break; }
+  if(val->left == nullptr && val->right != nullptr) { val->left = new BT(val); return; }
+  if(val->left != nullptr && val->right == nullptr)  { val->right = new BT(val); return; }
   if(val->left == nullptr && val->right == nullptr)  { cand = val; break; }
   q.push(val->left); q.push(val->right);
   q.pop();
