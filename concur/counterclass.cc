0389b1dbfbcb5907f25bbe8a44fb6ae6aeaa0b14
monut <deepakt@hotmail.com>
2018-09-09 04:18:23 -0700

    Update counterclass.cc

--- concur/counterclass.cc
+++ concur/counterclass.cc
@@ -3,13 +3,40 @@
   int get_val();
  */
  
- class CounterClass{
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <chrono>
+using namespace std;
+
+class CounterClass{
   private :
-    vector<double> cnt_;
-    double start_time_;
+    unsigned int cnt_;
+    vector<int> sum_; 
+    chrono::time_point<chrono::system_clock> start_time_;
+    mutex mtx_; // atomic<unsigned int> cnt_  no need for lock
+    double get_time(){
+         auto diff = (chrono:system_clock::now() - start_time_);
+         return duration_cast<chrono::minutes>(diff).count();
+    }
     
   public :
-    void inc(int val);
-    int get_val(int last_mins);  
+    CounterClass(){
+        start_time_ = chrono::system_clock::now(); cnt_ = 0};
+    
+    void inc(unsigned int val){
+         guard_lock<mutex> lck(mtx_);
+         cnt_ += val;
+         double tmnt  = get_time(); 
+         // atomic_exchange(sum_[tmnt], cnt_);
+         sum_[tmnt] = cnt_; 
+    };
+    
+    unsigned int get_val(unsigned int st_range, 
+                         unsigned int en_range ) {
+        return  (sum_[en_range] - sum_[st_range]);
+    }  
   
  }
+
+
