ef5326ff395ff3e161b86b8931106c12fb79658b
monut <deepakt@hotmail.com>
2018-09-16 18:38:43 -0700

    Create reverseSENTENCE.cc

---
+++ str/reverseSENTENCE.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// To execute C++, please define "int main()"
+
+
+int main() {
+    string str("bat cat");
+    reverse(str.begin(), str.end());
+    cout << str << endl;
+     
+    auto idx = str.find(" ");
+    size_t st = 0;
+    while(idx != string::npos){
+       reverse(str.begin()+ st, str.begin()+idx); 
+       st = idx +1;
+       idx = str.find(" ", st);
+       
+    }
+    
+    reverse(str.begin() + st, str.end());
+    
+    cout << str;
+}
