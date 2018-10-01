5635b29e3be114f3faae00ab08c73b1337b1f14c
monut <deepakt@hotmail.com>
2018-09-30 20:24:57 -0700

    Update merge_sort.cc

--- sort/merge_sort.cc
+++ sort/merge_sort.cc
@@ -2,6 +2,45 @@
 /*
  * Megre sort is stable sort for duplicates.
  */
+
+void mymerge(vector<int>& ar, int st, int m,  int en) {
+    int i = st;
+    int j = m+1;
+    vector<int> res;
+    while(i <= m && j <= en) {
+        if(ar[i] <= ar[j]){
+            res.push_back(ar[i++]);
+        } else {
+            res.push_back(ar[j++]);
+        }
+    }
+    while(i <= m){
+        res.push_back(ar[i++]);
+    }
+    
+    while(j <= en){
+        res.push_back(ar[j++]);
+    }
+    copy(res.begin(), res.end(), ar.begin() + st);
+    /*
+    for(int idx = st; idx <= en; idx++){
+        ar[idx] = res[idx-st];
+    }
+    */
+}
+void helper(vector<int>& intArr, int st, int en){
+    if(st >= en) return;
+    int mid = st + (en - st)/2;
+    helper(intArr, st, mid);
+    helper(intArr, mid+1, en);
+    mymerge(intArr, st, mid, en);
+}
+vector <int> MergeSort(vector <int> intArr) {
+    helper(intArr, 0, intArr.size() - 1);
+    return intArr;
+}
+
+
 void mymerge(vector<int>& ar, int st, int m,  int en) {
     int i = st;
     int j = m+1;
